056cfa03985eff23adbb22782cdd19422fab29cf
nenadam <nenadam@gmail.com>
2022-09-08 15:30:22 +0200

    https://recolnat.atlassian.net/browse/AO-3: i18n -> switch procet view, app startup alerts

--- app/containers/SwitchProject.js
+++ app/containers/SwitchProject.js
@@ -3,6 +3,7 @@ import {push} from "connected-react-router";
 import Component from '../components/SwitchProject';
 import {ee, EVENT_SELECT_TAB} from "../utils/library";
 import {setNewState} from "../actions/app";
+import { withTranslation } from 'react-i18next';
 
 const mapStateToProps = state => {
     return {
@@ -36,4 +37,4 @@ const mapDispatchToProps = dispatch => {
     };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Component);
+export default withTranslation()(connect(mapStateToProps, mapDispatchToProps)(Component));
