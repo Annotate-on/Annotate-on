4e749b4ee229a463d4dfaab31b4554e29b73d9b6
nenadam <nenadam@gmail.com>
2025-01-22 10:51:18 +0100

    code clean up

--- app/utils/xper_mono.js
+++ app/utils/xper_mono.js
@@ -92,8 +92,8 @@ export const searchItemsInKb = (filter, callback) => {
                             found.push(item);
                         }
                     }
-                    // found[0].name = "Polygonatum vulgare";
-                    // found[1].name = "Ficus";
+                    found[0].name = "Polygonatum vulgare";
+                    found[1].name = "Ficus";
                     callback(found);
                 } catch (e) {
                     callback(null);
